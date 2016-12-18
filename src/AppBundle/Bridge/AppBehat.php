c6a9c44c3f2fdb5f6c7978c4b9cfc7441077a6b3
athos99 <athos99@bluewin.ch>
2016-12-18 23:34:04 +0100

    dev

--- src/AppBundle/Bridge/AppBehat.php
+++ src/AppBundle/Bridge/AppBehat.php
@@ -6,6 +6,7 @@ use Behat\Behat\Definition\ServiceContainer\DefinitionExtension;
 use Behat\Behat\EventDispatcher\ServiceContainer\EventDispatcherExtension;
 use Behat\Behat\Gherkin\ServiceContainer\GherkinExtension;
 use Behat\Behat\Hook\ServiceContainer\HookExtension;
+use Behat\Behat\Output\Printer\Formatter\ConsoleFormatter;
 use Behat\Behat\Output\ServiceContainer\Formatter\JUnitFormatterFactory;
 use Behat\Behat\Output\ServiceContainer\Formatter\PrettyFormatterFactory;
 use Behat\Behat\Output\ServiceContainer\Formatter\ProgressFormatterFactory;
@@ -37,8 +38,10 @@ use Behat\Testwork\Tester\Result\IntegerTestResult;
 use Behat\Testwork\Tester\Result\TestWithSetupResult;
 use Behat\Testwork\Translator\ServiceContainer\TranslatorExtension;
 use SensioLabs\AnsiConverter\AnsiToHtmlConverter;
+
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\ConsoleOutput;
+
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -46,27 +49,29 @@ use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
 
-
-
-class MyOutputFactory extends OutputFactory {
+class MyOutputFactory extends OutputFactory
+{
 
     /** @var  OutputInterface */
     public $output;
-    public function __construct($out) {
-        $this->output=$out;
+
+    public function __construct($out)
+    {
+        $this->output = $out;
     }
 
     /**
      * @return OutputInterface
      */
-    public function createOutput() {
+    public function createOutput()
+    {
         return $this->output;
     }
 }
 
 
-class MyPrettyFormatterFactory extends PrettyFormatterFactory {
-
+class MyPrettyFormatterFactory extends PrettyFormatterFactory
+{
 
 
     public function __construct(ServiceProcessor $processor = null)
@@ -81,10 +86,14 @@ class MyPrettyFormatterFactory extends PrettyFormatterFactory {
      */
     protected function createOutputPrinterDefinition()
     {
-        return new Definition('Behat\Testwork\Output\Printer\StreamOutputPrinter', array(
-            new Definition('AppBundle\Bridge\myOutputFactory',
-            array( new Reference('cli.output'))
-        )));
+        return new Definition(
+            'Behat\Testwork\Output\Printer\StreamOutputPrinter', array(
+                new Definition(
+                    'AppBundle\Bridge\myOutputFactory',
+                    array(new Reference('cli.output'))
+                ),
+            )
+        );
     }
 }
 
@@ -113,11 +122,45 @@ class AppBehat
     }
 
 
+    public function getDefaultStyles()
+    {
+        return array(
+            'keyword' => new OutputFormatterStyle('black', 'white', array('bold')),
+            'stdout' => new OutputFormatterStyle('black', 'white', array()),
+            'exception' => new OutputFormatterStyle('red', 'white'),
+            'undefined' => new OutputFormatterStyle('yellow', 'white'),
+            'pending' => new OutputFormatterStyle('yellow', 'white'),
+            'pending_param' => new OutputFormatterStyle('yellow', 'white' ,array('bold')),
+            'failed'        => new OutputFormatterStyle('red', 'white'),
+            'failed_param'  => new OutputFormatterStyle('red', 'white', array('bold')),
+            'passed'        => new OutputFormatterStyle('green', 'white'),
+            'passed_param'  => new OutputFormatterStyle('green', 'white', array('bold')),
+            'skipped'       => new OutputFormatterStyle('cyan', 'white'),
+            'skipped_param' => new OutputFormatterStyle('cyan', 'white', array('bold')),
+            'comment'       => new OutputFormatterStyle('black', 'white'),
+            'tag'           => new OutputFormatterStyle('cyan', 'white')
+        );
+
+
+        $formatter = new ConsoleFormatter($this->isOutputDecorated());
+
+        foreach ($this->getDefaultStyles() as $name => $style) {
+            $formatter->setStyle($name, $style);
+        }
+
+    }
+
+
     public function init($rootPath)
     {
 
         $input = null;
-        $output = new BufferedOutput();
+        $output = new BufferedOutput(
+            BufferedOutput::VERBOSITY_NORMAL,
+            true,
+            new ConsoleFormatter(true, $this->getDefaultStyles())
+        );
+
 
         $this->rootPath = $rootPath;
 
@@ -156,7 +199,9 @@ class AppBehat
         $converter = new AnsiToHtmlConverter();
         $buffer = $output->fetch();
         $data = $converter->convert($buffer);
-return $data;
+        $data = nl2br($data);
+
+        return $data;
     }
 
 
@@ -253,7 +298,7 @@ return $data;
     {
         return array(
             new MyPrettyFormatterFactory($processor),
-   //         new PrettyFormatterFactory($processor),
+            //         new PrettyFormatterFactory($processor),
             new ProgressFormatterFactory($processor),
             new JUnitFormatterFactory(),
         );
@@ -261,3 +306,4 @@ return $data;
 
 
 }
+
